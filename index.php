e4bb59385b1c31d684b4b78bf911e70e414dcd47
Manisha <mmeena8261@gmail.com>
2021-12-20 12:32:21 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Online Food Ordering App</title>
+	<style>
+		a {
+		  display: inline;
+		  width: 15%;
+		  height: 5%;
+		  padding: 1%;
+		  margin-top: 15%;
+		  margin-left: 23%;
+		  margin-right: 0.1%;
+		  float: left;
+		  border: 1px solid blue;  
+		  background-color: black; 
+		  opacity:0.9;
+		}
+		
+		a{
+			color:white;
+			font-size:25px;
+		}
+		a:link {
+		  text-decoration: none;
+		 
+		}
+		a:hover {
+		  background-color:gray ;
+		}
+		body{
+			background-image: url(image/back.jpg);
+		}
+		
+		div{
+			color:white;
+			background-color:black;
+		}
+	</style>				
+</head>
+<body >
+
+<div>
+<hr>
+<h1 style="text-align:center;">Online Food Ordering System</h1>
+<hr>
+</div>
+
+	<a href="staff">Staff Management</a>
+	<a href="admin">Admin Management</a>
+</body>
+</html>
